95879df83cff1caf94e92f4eae689d862c3ba4aa
Edgar David <brenedgar12@gmail.com>
2024-05-04 20:44:57 -0600

    base Completa

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,13 @@ from api import views
 
 router = routers.DefaultRouter()
 router.register(r'tipoDocumento', views.TipoDocViewSet)
+router.register(r'institucion', views.InstitucionViewSet)
+router.register(r'departamento', views.DepartamentoViewSet)
+router.register(r'puesto', views.PuestoViewSet)
+router.register(r'uuario', views.UsuarioViewSet)
+router.register(r'documento', views.DocumentoViewSet)
+
+
 
 urlpatterns = [
     path('', include(router.urls))
